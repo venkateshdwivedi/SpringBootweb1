c76854a0fed4cedd6513d5e72c062e40dad67449
Venkatesh Dwivedi <dwivedivenkatesh98@gmail.com>
2025-06-26 22:20:33 +0530

    using spring way to accept data

--- src/main/java/com/example/springbootweb1/HomeController.java
+++ src/main/java/com/example/springbootweb1/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.springbootweb1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,13 +17,15 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(HttpServletRequest req, HttpSession session){
-        int num1= Integer.parseInt(req.getParameter("num1"));
-        int num2= Integer.parseInt(req.getParameter("num2"));
+    public String add(int num1,int num2, Model model){
+        // int num1= Integer.parseInt(req.getParameter("num1"));
+        // int num2= Integer.parseInt(req.getParameter("num2"));
 
         int result=num1+num2;
 
-        session.setAttribute("result", result); //data between pages is handled by session object
+        // session.setAttribute("result", result); //data between pages is handled by session object
+
+        model.addAttribute("result", result);
         
         return "result.jsp";
     }
--- src/main/webapp/result.jsp
+++ src/main/webapp/result.jsp
@@ -5,10 +5,12 @@
         <link rel="stylesheet" type="text/css" href="style.css">
 </head>
     <body>
-        <h2>Result is: <%=  session.getAttribute("result") %></h2>
+        <!-- <h2>Result is: <%=  session.getAttribute("result") %></h2> -->
 
         <!-- This is using JSTL -->
-        <!-- <h2>Result is: ${result}</h2>  -->
+        <h2>Result is: ${result} </h2> 
+
+        
 
     </body>
 </html>
